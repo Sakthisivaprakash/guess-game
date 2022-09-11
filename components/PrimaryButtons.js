f63040ff2b1eda406ef6dbf0e8740e6a8f01fb2d
Sakthisivaprakash <sakthisivaprakash@gmail.com>
2022-09-11 13:46:27 +0530

    Gloabl Color const approach

--- components/PrimaryButtons.js
+++ components/PrimaryButtons.js
@@ -1,11 +1,14 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
+
+import Colors from "../const/colors";
+
 const PrimaryButton = ({ children, onPress }) => {
   return (
     <View style={styles.buttonOuterCont}>
       <Pressable
         style={({pressed}) => pressed ? [styles.buttonInnerCont, styles.pressed] : styles.buttonInnerCont}
         onPress={onPress}
-        android_ripple={{ color: "#640233" }}
+        android_ripple={{ color: Colors.primary600 }}
       >
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
@@ -22,7 +25,7 @@ const styles = StyleSheet.create({
     overflow: "hidden",
   },
   buttonInnerCont: {
-    backgroundColor: "#72063c",
+    backgroundColor: Colors.primary500,
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 2,
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButtons";
+import Colors from "../const/colors";
 
 const StartGameScreen = ({OnConfirm}) => {
   const [enteredNumber, setEnteredNumber] = useState("");
@@ -59,7 +60,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#3b021f",
+    backgroundColor: Colors.primary800,
     borderRadius: 8,
     elevation: 4, // Shadow Prop for android
     shadowColor: "black", // Shadow Prop for IoS
@@ -72,8 +73,8 @@ const styles = StyleSheet.create({
     width: 50,
     fontSize: 32,
     borderBottomWidth: 2,
-    borderBottomColor: "#ddb52f",
-    color: "#ddb52f",
+    borderBottomColor: Colors.accent500,
+    color: Colors.accent500,
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center",
